4a7336700e5098aaa01650378e70da84a66a2334
Shirley Li <u1160160@umail.utah.edu>
2020-02-03 23:07:19 -0700

    Initial Commit and set up for A4

---
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -0,0 +1,48 @@
+﻿using SpreadsheetUtilities;
+using System;
+using System.Collections.Generic;
+
+namespace SS
+{
+    public class Spreadsheet : AbstractSpreadsheet
+    {
+        public Spreadsheet()
+        {
+            AbstractSpreadsheet sheet = new Spreadsheet();//create an empty spreadsheet
+        }
+        public override object GetCellContents(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<string> GetNamesOfAllNonemptyCells()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> SetCellContents(string name, double number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> SetCellContents(string name, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> SetCellContents(string name, Formula formula)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> SetCellContents(string name, Formula formula)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<string> GetDirectDependents(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
