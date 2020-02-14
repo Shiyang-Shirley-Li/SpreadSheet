8439c238a1860ab83236e3b216d00e11242fcd80
Shirley Li <u1160160@umail.utah.edu>
2020-02-14 15:30:59 -0700

    add tests

--- Spreadsheet/SpreadsheetTests/SpreadsheetTests.cs
+++ Spreadsheet/SpreadsheetTests/SpreadsheetTests.cs
@@ -399,6 +399,90 @@ namespace SpreadsheetTests
             sheet.SetContentsOfCell("A1", "= 1 + 0.5");
             Assert.AreEqual(1.5, sheet.GetCellValue("A1"));
         }
+
+        //Stress Test
+        [TestMethod()]
+        public void TestStress1()
+        {
+            Spreadsheet s = new Spreadsheet();
+            s.SetContentsOfCell("A1", "=B1+B2");
+            s.SetContentsOfCell("B1", "=C1-C2");
+            s.SetContentsOfCell("B2", "=C3*C4");
+            s.SetContentsOfCell("C1", "=D1*D2");
+            s.SetContentsOfCell("C2", "=D3*D4");
+            s.SetContentsOfCell("C3", "=D5*D6");
+            s.SetContentsOfCell("C4", "=D7*D8");
+            s.SetContentsOfCell("D1", "E1");
+            s.SetContentsOfCell("D2", "E1");
+            s.SetContentsOfCell("D3", "E1");
+            s.SetContentsOfCell("D4", "E1");
+            s.SetContentsOfCell("D5", "E1");
+            s.SetContentsOfCell("D6", "E1");
+            s.SetContentsOfCell("D7", "E1");
+            s.SetContentsOfCell("D8", "E1");
+            IList<string> list = new List<string> { "A1", "B1", "B2", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "E1" };
+            IList<string> testList = s.SetContentsOfCell("E1", "0");
+            testList.ToString();
+            bool equalityResult = true;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (!list[i].Equals(testList[i]))
+                {
+                    equalityResult = false;
+                }
+            }
+            Assert.IsTrue(equalityResult);
+        }
+
+        [TestMethod()]
+        public void TestStress2()
+        {
+            Spreadsheet s = new Spreadsheet();
+            ISet<String> cells = new HashSet<string>();
+            for (int i = 1; i < 200; i++)
+            {
+                cells.Add("A" + i);
+                Assert.IsTrue(cells.SetEquals(s.SetCellContents("A" + i, new Formula("A" + (i + 1)))));
+            }
+        }
+
+        [TestMethod()]
+        public void TestStress3()
+        {
+            Spreadsheet s = new Spreadsheet();
+            for (int i = 1; i < 200; i++)
+            {
+                s.SetCellContents("A" + i, new Formula("A" + (i + 1)));
+            }
+            try
+            {
+                s.SetCellContents("A150", new Formula("A50"));
+                Assert.Fail();
+            }
+            catch (CircularException)
+            {
+            }
+        }
+
+        [TestMethod()]
+        public void TestStress4()
+        {
+            Spreadsheet s = new Spreadsheet();
+            for (int i = 0; i < 500; i++)
+            {
+                s.SetCellContents("A1" + i, new Formula("A1" + (i + 1)));
+            }
+            HashSet<string> firstCells = new HashSet<string>();
+            HashSet<string> lastCells = new HashSet<string>();
+            for (int i = 0; i < 250; i++)
+            {
+                firstCells.Add("A1" + i);
+                lastCells.Add("A1" + (i + 250));
+            }
+            Assert.IsTrue(new HashSet<string>(s.SetCellContents("A1249", 25.0)).SetEquals(firstCells));
+            Assert.IsTrue(new HashSet<string>(s.SetCellContents("A1499", 0)).SetEquals(lastCells));
+        }
+
     }
 }
 
