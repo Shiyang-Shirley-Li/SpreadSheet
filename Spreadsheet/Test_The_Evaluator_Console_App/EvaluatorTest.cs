17a86b67c7a53ca03a11f3755be7b258ec747899
Shiyang(Shirley) Li <76845654+Shiyang-Shirley-Li@users.noreply.github.com>
2021-01-08 19:19:22 -0700

    Update EvaluatorTest.cs

--- Spreadsheet/Test_The_Evaluator_Console_App/EvaluatorTest.cs
+++ Spreadsheet/Test_The_Evaluator_Console_App/EvaluatorTest.cs
@@ -3,8 +3,7 @@ using System;
 /// <summary>
 /// Author: Shiyang(Shirley) Li
 /// Date:01/11/2020
-/// Course: CS 3500, University of Utah, School of Computing
-/// Copyright: CS 3500 and Shiyang(Shirley) Li - This work may not be copied for use in Academic Coursework.
+/// Copyright:This work may not be copied for use in Academic Coursework.
 /// 
 /// I, Shiyang(Shirley) Li, certify that I wrote this code from scratch and did not copy it in part or whole from  
 /// another source.  All references used in the completion of the assignment are cited in my README file. 
