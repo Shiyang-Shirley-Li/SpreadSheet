98583629925825725b4c03d5a2652b832f3b6820
Shiyang(Shirley) Li <76845654+Shiyang-Shirley-Li@users.noreply.github.com>
2021-01-08 19:22:39 -0700

    Update Spreadsheet.cs

--- Spreadsheet/Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -1,11 +1,10 @@
 ﻿/// <summary>
 /// Author: Shiyang(Shirley) Li
 /// Date:02/10/2020
-/// Course: CS 3500, University of Utah, School of Computing
-/// Copyright: CS 3500 and Shiyang(Shirley) Li - This work may not be copied for use in Academic Coursework.
+/// Copyright:Shiyang(Shirley) Li - This work may not be copied for use in Academic Coursework.
 /// 
 /// I, Shiyang(Shirley) Li, certify that I wrote this code from scratch and did not copy it in part or whole from  
-/// another source.  All references used in the completion of the assignment are cited in my README file. 
+/// another source.  All references used in the completion of the code are cited in my README file. 
 /// 
 /// This file is a method that build the "internals" of the spreadsheet
 /// 
